6977b5830a5aa7e66fc4fe3359db3bbde1f9f917
Rafael Padilla <carlos09.cp65@gmail.com>
2021-01-13 20:12:35 -0600

    Finish

--- src/server/index.js
+++ src/server/index.js
@@ -1,10 +1,7 @@
 require('ignore-styles');
 
 require('@babel/register')({
-  presets: [
-    '@babel/preset-env',
-    '@babel/preset-react',
-  ],
+  presets: ['@babel/preset-env', '@babel/preset-react'],
 });
 
 require('asset-require-hook')({
